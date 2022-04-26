f0815a132bfefeaea9a12d42e360a4a9fb7f9d81
kjs29 <96529477+kjs29@users.noreply.github.com>
2022-04-25 20:48:47 -0500

    Create Enumeration.cpp

---
+++ Enumeration.cpp
@@ -0,0 +1,5 @@
+An enumeration is a user-defined data type that consists of integral constants.
+---example
+
+enum season {spring, summer, fall, winter};
+name of this enumeration is 'season'. And 'spring','summer','fall','winter' are value.
