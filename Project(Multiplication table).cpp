e5fbc40d2ad2d2166f602cc48155037d9437c397
kjs29 <96529477+kjs29@users.noreply.github.com>
2022-04-26 09:20:12 -0500

    Create Project(Multiplication table).cpp

---
+++ Project(Multiplication table).cpp
@@ -0,0 +1,15 @@
+//multiplication table
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	for (int i=1;i<=9;i++){
+		cout<<i<<"\n";
+		for (int j =1; j<=9;j++){
+		cout<<i<<" * "<<j<<" = ";
+		cout<< i*j<<"\n";	
+		}
+
+	}
+}
