6fd4753bcf35d9ef632a196466bb9eb43cce995b
kjs29 <96529477+kjs29@users.noreply.github.com>
2022-04-27 14:54:12 -0500

    Update Array.cpp

--- Array.cpp
+++ Array.cpp
@@ -1,3 +1,27 @@
+//Definition
+Arrayes allow us to create multiple variables at once.
+These variables are assigned in specific place.
+
+//how many bytes usually go in each block of memory	
+int = 4 bytes
+float = 4 bytes
+double = 8 bytes
+
+//initialization
+int arrayName [10];
+
+//if we don't initialize specific values, garbage values go in there
+#include <iostream>
+using namespace std;
+int main () {
+	int hello[10];
+	cout<<hello[5];
+}
+result:
+
+//this is the garbage value that has been stored in those arrays that we didn't assign values.
+4199721
+---
 Syntax for creating array(Arrays can contain only the same data type.)
 int arrayName [size] = {element1, element2, element3 };
 
@@ -28,7 +52,7 @@ int main () {
 	int primeYear[5];
 	
 	cout << "Your prime years will be : \n"; 
-	
+	cout<<primeYear[0];
 	for (int i = 0;i<5;i++) {
 	
 		primeYear[i] = 2022 + i;
@@ -37,7 +61,53 @@ int main () {
 	
 	return 0;
 }
+---
+	
+//we can assign '0' in all index position in an array
+#include <iostream>
+using namespace std;
+int main () {
+	
+	int helloArray[5]= {};
+	cout<<helloArray[0]<<"\n";
+	cout<<helloArray[1]<<"\n";
+	cout<<helloArray[2]<<"\n";
+	cout<<helloArray[3]<<"\n";
+	cout<<helloArray[4]<<"\n";
+	cout<<helloArray[5]<<"\n";
+	
+	return 0;
+}
 
+result:
+0
+0
+0
+0
+0
+0
+---
+//we can also give value of '0' to other indexes in an array
+#include <iostream>
+using namespace std;
+int main () {
+	
+	//this means hello[0],hello[1],hello[2] are assigned 1,2,3 and the rest indexes are assigned '0'.
+	int hello[5] = {1,2,3};
+	
+	
+	//to find out
+	cout<<hello[0];
+	cout<<hello[1];
+	cout<<hello[2];
+	cout<<hello[3];
+	cout<<hello[4];
+	cout<<hello[5];
+	return 0;
+}
+
+result:
+123000
 ---
 //we can also initialize an array without specifying the size of an array
 #include <iostream>
@@ -74,13 +144,35 @@ return 0;
 
 ---
 //Creating a multidimensional arrays
+//2D array means there are 2 [][], 3D array has 3[][][]
+//Coordinates.
 /*
-string letters[2][4]; 		//this one is to declare multidimensional array without giving vaues.
-string letters[2][4] = {	//this one is to declare multidimensional array assigning values.
+
+string letters[2][4]; 		//this one is to declare multidimensional array without giving vaues. All we know is there are 8 garbage value assigned.
+
+string letters[2][4] = {	//this one is to declare multidimensional array assigning values. We know what the values are, and there are 8 of them.
 {"a","b","c","d"},
 {"e","f","g","h"}
 }
 */
+---
+//when we want to print all individual values in multidimentional arrays we can use double 'for.
+#include<iostream>
+using namespace std;
+int main () {
+	//There are 100 values assigned all 0, except for hello[0][1],hello[1][0],hello[1][1]
+	int hello[10][10] = {{1},{5,3}};
+	for (int i=0;i<10;i++){
+		for(int j =0;j<10;j++){
+			cout<<hello[i][j];
+		}
+	}
+	return 0;
+}
+
+result:
+1000000000530000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+	
 ---example
 #include <iostream>
 int main () {
@@ -97,3 +189,6 @@ int main () {
 	
 	return 0;
 }
+
+result:
+abcdefgh
