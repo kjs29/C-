baedfe7a2d7fc5b769571bad0f1ac0d262319d70
kjs29 <96529477+kjs29@users.noreply.github.com>
2022-04-28 13:04:21 -0500

    Create Type casting operator.cpp

---
+++ Type casting operator.cpp
@@ -0,0 +1,17 @@
+'Type Casting Operators' allow to convert a value of a given type to another type.
+We use parenthesis '()'.
+  
+---example
+//For example, if we want to convert float f int an integer.
+#include <iostream>
+using namespace std;
+int main () { 
+    float f = 3.14;
+  
+    //i has to be declared to an integer though.
+    i = (int)f;
+    cout<<i;
+}
+
+result :
+3       //the remainder is lost.
